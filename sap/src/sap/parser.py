eff21e03805b2971b57a9c19db1986b045c948e9
Radu Grigore <radugrigore@gmail.com>
2010-11-19 14:41:46 +0000

    Nicer lexing.
    - I use Python generators so that the code is nicer.
    - tracking locations
    - treat tabs
    - glue lines (ending in \)
    - strip trailing space (which otherwise may confuse the indent processor)

--- sap/src/sap/parser.py
+++ sap/src/sap/parser.py
@@ -1,12 +1,32 @@
 """A parser for SAP scripts.
 """
 
-# BIG TODO: Location.
-
 import bisect
 
 class NoMatchError(Exception):
   pass
+
+class Location:
+  """The start and end are (line, column) pairs. Inclusive.
+  """
+  def __init__(self, start, end=None):
+    self.start = start
+    self.end = end if end is not None else start
+
+  def __str__(self):
+    def pp(x):
+      return '{0}:{1}'.format(x[0], x[1])
+    ps = pp(start)
+    pe = pp(end)
+    return ps + '-' + pe if ps != pe else ps
+
+def join_locations(locations):
+  pass # TODO
+
+class DataWithLocation:
+  def __init__(self, data, location):
+    self.data = data
+    self.location = location
 
 def parse(rules, stream):
   for to_match, value in rules:
@@ -24,29 +44,75 @@ def parse(rules, stream):
       stream.rewind()
   return None
 
-class CommentStripper:
-  def __init__(self, input):
-    self.input = input
-
-  def __iter__(self):
-    return self
-
-  def __next__(self):
-    c = next(self.input)
-    if c == '#':
-      return self.hide()
+def add_locations(input):
+  line = 1
+  column = 1
+  for c in input:
+    position = (line, column)
+    yield DataWithLocation(c, Location(position))
+    if c == '\n':
+      line += 1
+      column = 1
     else:
-      return c
+      column += 1
 
-  def hide(self):
-    while True:
-      c = next(self.input)
-      if c == '\n':
-        return '\n'
+def strip_comments(input):
+  state = 0
+  for t in input:
+    if state == 0:
+      if t.data == '#':
+        state = 1
+      else:
+        yield t
+    else:
+      if t.data == '\n':
+        state = 0
+        yield t
 
-class LineProcessor:
-  pass
-  #TODO: eat '\n ' and empty lines
+def space_of_tab(input):
+  for t in input:
+    if t.data == '\t':
+      sys.stderr.write('tab at {0} treated as space\n'
+          .format(t.location)) # TODO
+      t.data = ' '
+    yield t
+
+def glue_lines(input):
+  state = 0
+  for t in input:
+    if state == 0:
+      if t.data == '\\':
+        escape = t
+        state = 1
+      else:
+        yield t
+    else:
+      if t.data != '\n':
+        yield escape
+        yield t
+      state = 0
+
+def add_newline_at_end(input):
+  for t in input:
+    yield t
+  if t.data != '\n':
+    position = (t.location.end[0], t.location.end[1] + 1)
+    yield DataWithLocation('\n', Location(position))
+
+def strip_trailing_space(input):
+  queue = []
+  for t in input:
+    if t.data == ' ':
+      queue.append(t)
+    else:
+      if t.data != '\n':
+        for q in queue:
+          yield q
+      queue = []
+      yield t
+
+def process_indents(input):
+  pass # TODO
 
 class IndentProcessor:
   def __init__(self, input):
@@ -76,6 +142,13 @@ class IndentProcessor:
 
 if __name__ == '__main__':
   import sys
-  parser = IndentProcessor(CommentStripper(iter(sys.stdin.read())))
-  for c in parser:
+  cs = iter(sys.stdin.read())  # TODO: don't load in memory
+  cs = add_locations(cs)
+  cs = add_newline_at_end(cs)
+  cs = space_of_tab(cs)
+  cs = strip_comments(cs)
+  cs = glue_lines(cs)  # NOTE: in C usually this is done before strip_comments
+  cs = strip_trailing_space(cs)
+  cs = map(lambda x: x.data, cs)
+  for c in cs:
     sys.stdout.write(c)
